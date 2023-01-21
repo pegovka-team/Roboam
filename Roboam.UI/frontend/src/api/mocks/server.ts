3c8f4a1b6e8fa550ae086366419564e2815808bd
d.stukov <d.stukov@skbkontur.ru>
2023-01-21 14:14:48 +0500

    implement algorithmData contract for mock server

--- Roboam.UI/frontend/src/api/mocks/server.ts
+++ Roboam.UI/frontend/src/api/mocks/server.ts
@@ -2,6 +2,7 @@ import jsonServer from "json-server";
 import { fakeRouter } from "./router";
 import { routing } from "./routing";
 import { algorithmNames, algorithmsDataList, detailsLevels, favoriteTasks, maxTaskNumber, tagsNames } from "./db";
+import { IAlgorithmData } from "../../models/algorithm-data";
 
 const server = jsonServer.create();
 const router = jsonServer.router(fakeRouter);
@@ -14,10 +15,44 @@ server.use((_req, res, next) => {
 
 server.use(jsonServer.rewriter(routing));
 
-server.get("/algorithmData", function (_req, res) {
+server.get("/algorithmData", function (req, res) {
+    let notFilteredData = algorithmsDataList;
+
+    const tags = req.query.tags;
+    if (typeof tags === 'string') {
+        notFilteredData = notFilteredData.filter(x => x.tags.includes(tags));
+    } else if (Array.isArray(tags) && tags.length) {
+        notFilteredData = notFilteredData.filter(x => tags.some(tag => x.tags.includes(tag.toString())));
+    }
+
+    const algorithmNames = req.query.algorithmNames;
+    if (algorithmNames === undefined) {
+        // get best result from all possible results
+        const groupedByTaskNumberData: {[taskId: number]: IAlgorithmData[]} = {}
+        for (const data of notFilteredData) {
+            if (groupedByTaskNumberData[data.taskNumber]) {
+                groupedByTaskNumberData[data.taskNumber].push(data);
+            } else {
+                groupedByTaskNumberData[data.taskNumber] = [data];
+            }
+        }
+
+        const result = [];
+        for (const [_, data] of Object.entries(groupedByTaskNumberData)) {
+            const dataWithBestAlgorithmMax = data.reduce((prev, current) => (prev.algorithmMax > current.algorithmMax) ? prev : current);
+            result.push(dataWithBestAlgorithmMax);
+        }
+        notFilteredData = result;
+    } else if (Array.isArray(algorithmNames) && algorithmNames?.length) {
+        const names = algorithmNames.map(a => a.toString());
+        notFilteredData = notFilteredData.filter(a => names.includes(a.algorithmName));
+    } else if (typeof req.query.algorithmNames === 'string') {
+        notFilteredData = notFilteredData.filter(a => req.query.algorithmNames === a.algorithmName);
+    }
+
     return res.jsonp({
         isSuccessful: true,
-        result: algorithmsDataList
+        result: notFilteredData
     });
 });
 
