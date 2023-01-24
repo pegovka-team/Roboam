e032b6021fde61199c91b8d28ca05a2afa5501dc
Denis Avriskin <teewar2@yandex.com>
2023-01-24 14:40:43 +0500

    add missing changes for "a lot of changes from previous sprints" commit :/

--- Roboam.Agent/RunWorkerCommands.cs
+++ Roboam.Agent/RunWorkerCommands.cs
@@ -1,11 +1,46 @@
+using System;
+using System.Linq;
+
 namespace agent
 {
     public class RunWorkerCommands
     {
-        public string? BuildArgs { get; set; }
-        public string? BuildCommand { get; set; }
+        public RunWorkerCommands(string[] commands)
+        {
+            Commands = commands
+                .Where(command => command.Length > 0)
+                .Select(command => new RunWorkerCommand(command)).ToArray();
+        }
 
-        public string ExecuteProcessArgs { get; set; }
-        public string ExecuteProcessCommand { get; set; }
+        public RunWorkerCommands()
+        {
+            Commands = Array.Empty<RunWorkerCommand>();
+        }
+
+        public RunWorkerCommand[] Commands;
+    }
+
+    public class RunWorkerCommand
+    {
+        public RunWorkerCommand(string command)
+        {
+            if (command.StartsWith("@")) // TODO: заменить разметку для вставки коммитных аргументов на $1, $2, $*, ...
+            {
+                WithArgsFromCommit = true;
+                command = command[1..];
+            }
+            else
+            {
+                WithArgsFromCommit = false;
+            }
+
+            var executableAndArgs = command.Split(' ', 2);
+            Executable = executableAndArgs[0];
+            Args = executableAndArgs.Length > 1 ? executableAndArgs[1] : "";
+        }
+
+        public string Executable;
+        public string Args;
+        public bool WithArgsFromCommit;
     }
 }
--- Roboam.Agent/WorkerExecuter.cs
+++ Roboam.Agent/WorkerExecuter.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CliWrap;
-using CliWrap.Buffered;
 
 namespace agent
 {
